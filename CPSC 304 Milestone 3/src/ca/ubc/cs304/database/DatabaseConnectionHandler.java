dc34039e254d8aeca84a588628a0c6eecea4f0c7
ethanyichen <Easonzyc_@outlook.com>
2020-11-11 13:02:11 -0800

    1.0 Original Upload

---
+++ CPSC 304 Milestone 3/src/ca/ubc/cs304/database/DatabaseConnectionHandler.java
@@ -0,0 +1,103 @@
+package ca.ubc.cs304.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+/**
+ * This class handles all database related transactions
+ */
+public class DatabaseConnectionHandler {
+	// Use this version of the ORACLE_URL if you are running the code off of the server
+//	private static final String ORACLE_URL = "jdbc:oracle:thin:@dbhost.students.cs.ubc.ca:1522:stu";
+	// Use this version of the ORACLE_URL if you are tunneling into the undergrad servers
+	private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
+	private static final String EXCEPTION_TAG = "[EXCEPTION]";
+	private static final String WARNING_TAG = "[WARNING]";
+	
+	private Connection connection = null;
+	
+	public DatabaseConnectionHandler() {
+		try {
+			// Load the Oracle JDBC driver
+			// Note that the path could change for new drivers
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		} catch (SQLException e) {
+			System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+		}
+	}
+	
+	public void close() {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+		}
+	}
+
+
+	
+	public boolean login(String username, String password) {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+	
+			connection = DriverManager.getConnection(ORACLE_URL, username, password);
+			connection.setAutoCommit(false);
+	
+			System.out.println("\nConnected to Oracle!");
+			return true;
+		} catch (SQLException e) {
+			System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+			return false;
+		}
+	}
+
+	private void rollbackConnection() {
+		try  {
+			connection.rollback();	
+		} catch (SQLException e) {
+			System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+		}
+	}
+	
+	public void databaseSetup() {
+		dropBranchTableIfExists();
+		
+		try {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("");
+			stmt.close();
+		} catch (SQLException e) {
+			System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+		}
+
+	}
+	
+	private void dropBranchTableIfExists() {
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("select table_name from user_tables");
+			
+			while(rs.next()) {
+				if(rs.getString(1).toLowerCase().equals("...")) {
+					stmt.execute("DROP TABLE ...");
+					break;
+				}
+			}
+			
+			rs.close();
+			stmt.close();
+		} catch (SQLException e) {
+			System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+		}
+	}
+}
