5611d6b2fe8ff3af2bb5ab9b438054cb02ad2792
ethan <Easonzyc_@outlook.com>
2020-11-17 18:16:42 -0800

    2.5 # Added database setup and drop table sql

--- CPSC 304 Milestone 3/src/ca/ubc/cs304/sql/databaseSetup.sql
+++ CPSC 304 Milestone 3/src/ca/ubc/cs304/sql/databaseSetup.sql
@@ -1,1 +1,131 @@
+CREATE TABLE Cart
+(
+    cartID INTEGER,
+    model  VARCHAR(5),
+    PRIMARY KEY (cartID)
+);
+CREATE TABLE Warehouse
+(
+    warehouseID INTEGER,
+    location    VARCHAR(50),
+    wName       VARCHAR(50),
+    PRIMARY KEY (warehouseID)
+);
+CREATE TABLE Employee
+(
+    employeeID  INTEGER,
+    warehouseID INTEGER NOT NULL,
+    eName       VARCHAR(80),
+    PRIMARY KEY (employeeID),
+    FOREIGN KEY (warehouseID) REFERENCES Warehouse (warehouseID)
+);
+CREATE TABLE employeeSalary
+(
+    employeeID INTEGER,
+    salary     INTEGER,
+    PRIMARY KEY (employeeID),
+    FOREIGN KEY (employeeID) REFERENCES Employee (employeeID) ON DELETE CASCADE
+);
+CREATE TABLE use
+(
+    cartID     INTEGER,
+    employeeID INTEGER,
+    startTime  DATE,
+    endTime    DATE,
+    PRIMARY KEY (cartID, employeeID),
+    FOREIGN KEY (employeeID) REFERENCES Employee (employeeID) ON DELETE SET NULL
+);
+CREATE TABLE Customer
+(
+    customerID INTEGER,
+    cName      VARCHAR(80),
+    phoneNum   CHAR(10),
+    PRIMARY KEY (customerID)
+);
 
+CREATE TABLE Unit
+(
+    unitID      INTEGER,
+    capacity    INTEGER,
+    warehouseID INTEGER NOT NULL,
+    employeeID  INTEGER NOT NULL,
+    customerID  INTEGER,
+    startDate   DATE,
+    endDate     DATE,
+    PRIMARY KEY (unitID),
+    FOREIGN KEY (customerID) REFERENCES Customer (CustomerID) ON DELETE SET NULL,
+    FOREIGN KEY (employeeID) REFERENCES Employee (employeeID),
+    FOREIGN KEY (warehouseID) REFERENCES Warehouse (warehouseID)
+);
+
+CREATE TABLE ParkingSpace
+(
+    parkNum    INTEGER,
+    customerID INTEGER,
+    parkSince  Date,
+    UNIQUE (customerID),
+    PRIMARY KEY (parkNum),
+    FOREIGN KEY (customerID) REFERENCES Customer (customerID) ON DELETE SET NULL
+);
+CREATE TABLE Box
+(
+    boxID      INTEGER,
+    unitID     INTEGER NOT NULL,
+    customerID INTEGER NOT NULL,
+    boxSize       INTEGER,
+    PRIMARY KEY (boxID),
+    FOREIGN KEY (unitID) REFERENCES Unit (UnitID),
+    FOREIGN KEY (customerID) REFERENCES Customer (CustomerID)
+);
+CREATE TABLE Own
+(
+    warehouseID INTEGER,
+    parkNum     INTEGER,
+    PRIMARY KEY (warehouseID, parkNum),
+    FOREIGN KEY (warehouseID) REFERENCES Warehouse (warehouseID) ON DELETE SET NULL,
+    FOREIGN KEY (parkNum) REFERENCES ParkingSpace (parkNum)
+);
+CREATE TABLE Member
+(
+    warehouseID         INTEGER NOT NULL,
+    customerID          INTEGER,
+    membershipStartDate DATE,
+    PRIMARY KEY (warehouseID, customerID),
+    FOREIGN KEY (warehouseID) REFERENCES Warehouse (warehouseID) ON DELETE CASCADE,
+    FOREIGN KEY (customerID) REFERENCES Customer (customerID) ON DELETE CASCADE
+);
+CREATE TABLE rentFee
+(
+    startDate  DATE,
+    capacity   INTEGER,
+    monthlyFee INTEGER,
+    PRIMARY KEY (startDate, capacity)
+);
+CREATE TABLE Room
+(
+    unitID  INTEGER,
+    roomNum INTEGER,
+    PRIMARY KEY (unitID)
+);
+CREATE TABLE Container
+(
+    unitID       INTEGER,
+    containerNum INTEGER,
+    PRIMARY KEY (unitID)
+);
+CREATE TABLE Uniform
+(
+    uniformID  INTEGER,
+    employeeID INTEGER,
+    PRIMARY KEY (uniformID, employeeID),
+    FOREIGN KEY (employeeID) REFERENCES Employee (employeeID)
+);
+CREATE TABLE purchase
+(
+    warehouseID INTEGER NOT NULL,
+    cartID      INTEGER,
+    since       DATE,
+    PRIMARY KEY (warehouseID, cartID),
+    FOREIGN KEY (warehouseID) REFERENCES Warehouse (warehouseID) ON DELETE SET NULL,
+    FOREIGN KEY (cartID) REFERENCES Cart (cartID)
+);
---
+++ CPSC 304 Milestone 3/src/ca/ubc/cs304/sql/dropAll.sql
@@ -0,0 +1,17 @@
+DROP TABLE Cart;
+DROP TABLE Warehouse;
+DROP TABLE Employee;
+DROP TABLE employeeSalary;
+DROP TABLE use;
+DROP TABLE Customer;
+DROP TABLE Unit;
+DROP TABLE ParkingSpace;
+DROP TABLE Box;
+DROP TABLE Own;
+DROP TABLE Member;
+DROP TABLE rentFee;
+DROP TABLE Room;
+DROP TABLE Container;
+DROP TABLE Uniform;
+DROP TABLE purchase;
+
